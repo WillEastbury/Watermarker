a15cb251bd640e3479edcc6627c0fca388d80525
Will Eastbury <willeastbury@gmail.com>
2024-02-02 09:48:42 +0000

    initial commit

--- ApplyWaterMark.cs
+++ ApplyWaterMark.cs
@@ -8,37 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using ImageMagick;
 using System.Net.Http;
-
 public static class ApplyWatermark
 {
     [FunctionName("ApplyWatermark")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "WaterMarker/{imageUrl}/{x}/{y}/{watermarkText}/{outputFileName}")] HttpRequest req, 
+        string imageUrl, int x, int y, string watermarkText, string outputFileName,
         ILogger log)
     {
-        log.LogInformation("C# HTTP trigger function processed a request.");
-
-        // Get URL and text from the request
-        string imageUrl = req.Form["imageUrl"];
-        string watermarkText = req.Form["watermarkText"];
-
-        // Download image from URL
+        log.LogInformation("C# HTTP trigger function processed a request for a watermark.");
         byte[] imageBytes;
         using (HttpClient client = new HttpClient())
         {
             imageBytes = await client.GetByteArrayAsync(imageUrl);
         }
-
-        // Apply watermark using ImageMagick.NET
         using (MemoryStream inputStream = new MemoryStream(imageBytes))
         {
             using (MagickImage image = new MagickImage(inputStream))
             {
-                IMagickGeometry magickGeometry = new MagickGeometry(0, 0, image.Width, image.Height);
-                // Add watermark text
+                IMagickGeometry magickGeometry = new MagickGeometry(x, y, image.Width, image.Height);
                 image.Annotate(watermarkText, magickGeometry, Gravity.South);
-
-                // Save the watermarked image
                 using (MemoryStream outputStream = new MemoryStream())
                 {
                     image.Write(outputStream);
